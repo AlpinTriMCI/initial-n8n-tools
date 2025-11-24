686f4296af5189fa57d887dfce81aa234b79cef0
Alpin Tri <alpintripranjadata@gmail.com>
2025-11-24 10:42:01 +0700

    Adjust domain name and webhook url for test

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 HOSTNAME=$(hostname)
-# DOMAIN="${HOSTNAME}.sandboxwork.my.id" # Set domain here
-# WEBHOOK_URL="https://api.devstech.web.id/webhooks"
-DOMAIN="${HOSTNAME}.alpintripranjadata.my.id" # Set domain here
+DOMAIN="${HOSTNAME}.sandboxwork.my.id" # Set domain here
 WEBHOOK_URL="https://rachel.devstech.web.id/api/v1/compute-webhooks/installation-progress"
+# WEBHOOK_URL="https://api.devstech.web.id/webhooks"
+# DOMAIN="${HOSTNAME}.alpintripranjadata.my.id" # Set domain here
 
 # Helper send progress
 send_progress() {
