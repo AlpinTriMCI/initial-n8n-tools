41d766770a3ef72735025a8fd21dd81311de8a39
Alpin Tri <alpintripranjadata@gmail.com>
2025-11-25 09:08:55 +0700

    switch to dns challenge

--- install.sh
+++ install.sh
@@ -81,6 +81,10 @@ SUBDOMAIN=n8n
 
 # The email address to use for the TLS/SSL certificate creation
 SSL_EMAIL=user@${DOMAIN}
+
+# Cloudflare token
+CLOUDFLARE_DNS_API_TOKEN=Gen6jIT1U0drAeWto00XbtwVj5hnDsNkMvXGrSko
+CLOUDFLARE_EMAIL=alpintripranjadata@gmail.com
 EOF
 
 # Run docker compose
